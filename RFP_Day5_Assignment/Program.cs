50e9d2042fdf5c6ac9c23089a09f78639e008f4c
Ankan Sarkar <74021101+Ankansa@users.noreply.github.com>
2022-05-18 01:00:48 +0530

    Add files via upload

---
+++ RFP_Day5_Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RFP_Day5_Assignment;
+Console.WriteLine("Hello, World!");
+FlipCoin.flipCoinMethod();
+Leapyear.Yearmethod();
+Powof2.method();
+HarmonicSeries.harmonicMethod();
+Factors.primeFactorsMethod();
+QuotientRemainder.quotientRemainderMethod();
+SwapNumber.swapMethod();
+EvenOdd.evenOddMethod();
+AlphabetVowelConsonant.vowelConsonantMethod();
+LargestThreeNumber.bubbleSortMethod();
